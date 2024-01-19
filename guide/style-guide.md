bd80ecfcbc5134884c22f1e360389e8fc4c0a553
safrilth <safriltaufiqhidayat@gmail.com>
2024-01-19 10:51:22 +0700

    final

---
+++ guide/style-guide.md
@@ -0,0 +1,37 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+--clr-primary-1: hsl(75, 94%, 57%);
+--clr-neutral-1: hsl(0, 0%, 100%);
+--clr-neutral-2: hsl(0, 0%, 20%);
+--clr-neutral-3: hsl(0, 0%, 12%);
+--clr-neutral-4: hsl(0, 0%, 8%);
+--fs: 14px;
+--ff: "Inter";
+--fw-light: 400;
+--fw-medium: 600;
+--fw-bold: 700;
+
+### Neutral
+
+
+## Typography
+
+### Body Copy
+
+- Font size (paragraph): 14px
+
+### Font
+
+- Family: [Inter](https://fonts.google.com/specimen/Inter)
+- Weights: 400, 600, 700
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,77 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import avatar from "$lib/assets/images/avatar-jessica.jpeg"
+</script>
+
+<article class="card">
+  <header class="profile">
+    <img src={avatar} alt="jesica avatar" >
+    <h1>Jessica Randall</h1>
+    <h4>London, United Kingdom</h4>
+    <p>"Front-end developer and avid reader"</p>
+  </header>
+  <section class="social-list">
+    <button>Github</button>
+    <button>Frontend Mentor</button>
+    <button>LinkedIn</button>
+    <button>Twitter</button>
+    <button>Instagram</button>
+  </section>
+</article>
+
+<style>
+  /* Layout */
+  .card {
+    background-color: var(--clr-neutral-3);
+    border-radius: 1rem;
+    overflow: hidden;
+    /* margin-inline: 1rem; */
+    padding: 2rem;
+    margin: 2rem;
+    max-width: 30rem;
+    display: grid;
+    grid-template-rows: 2fr 3fr;
+    text-align: center;
+  }
+
+  .profile {
+    display: grid;
+    gap: 0.8rem;
+    justify-items: center;
+  }
+  
+  .social-list {
+    margin-top: 1rem;
+    display: grid;
+    gap: 1rem;
+    /* align-items: center; */
+    /* padding: 2rem; */
+  }
+
+  /* Component */
+  img {
+    border-radius: 50%;
+    width: 40%;
+  }
+
+  h4 {
+    color: var(--clr-primary-1);
+  }
+
+  p {
+    font-size: small;
+  }
+
+  button {
+    border-radius: 0.5rem;
+    background-color: var(--clr-neutral-2);
+    border: transparent;
+    color: var(--clr-neutral-1);
+    font-weight: var(--fw-bold);
+  }
+
+  button:hover{
+    background-color: var(--clr-primary-1);
+    color: var(--clr-neutral-2)
+  }
+  
+</style>
